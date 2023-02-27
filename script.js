7c206eaf926a576201d787788f6e3876eae4ba32
anishaparvez <113203774+anishaparvez@users.noreply.github.com>
2023-02-27 13:46:21 -0500

    save

---
+++ script.js
@@ -0,0 +1,5 @@
+const boxs = document.querySelectorAll(".box");
+const statusTxt = document.querySelector("#status");
+const btnRestart = document.querySelector("#restart");
+let x = "<img src='images/x.png'>";
+let o = "<img src='images/o.png'>";
